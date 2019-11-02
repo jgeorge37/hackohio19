29cf34a8cd3203cd986fb5189905cd400c3c486a
hooveranna <15109376+hooveranna@users.noreply.github.com>
2019-11-02 15:10:42 -0400

    Add files via upload

---
+++ IActionState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackGame.State;
+
+namespace HackGame.States.Avatar_States
+{
+    interface IActionState : IState
+    {
+        IActionState MoveRight();
+        IActionState Jump();
+        IActionState Exit();
+    }
+}
