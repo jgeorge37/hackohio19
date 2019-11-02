cee48cd16a6187a9c0b14a9c3fdbfab7eea7dc51
hooveranna <15109376+hooveranna@users.noreply.github.com>
2019-11-02 18:44:46 -0400

    adding collisions

--- FinalAttempt/Game8/Avatar.cs
+++ FinalAttempt/Game8/Avatar.cs
@@ -5,76 +5,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Game8.Commands;
+using Game8.Collisions;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace Game8
 {
-    class Avatar
+    class Avatar : ICollidable
     {
-				bool isJumping;
+        bool isJumping;
         int heightJumped;
         int maxJumpHeight;
-				Animation JumpUp;
-				Animation CurrentAnimation;
+        Animation JumpUp;
+        Animation CurrentAnimation;
 
-		    public Avatar(GraphicsDevice graphicsDevice)
-		    {
-						isJumping = false;
-        		heightJumped = 0;
+        public Avatar(GraphicsDevice graphicsDevice)
+        {
+            isJumping = false;
+            heightJumped = 0;
             maxJumpHeight = 100;
 
-						/*
-						THIS IS STUFF FROM THE EXAMPLE THAT IDK HOW TO DEAL WITH
+            /*
+            THIS IS STUFF FROM THE EXAMPLE THAT IDK HOW TO DEAL WITH
 
-						if (characterSheetTexture == null)
-					  {
-					      using (var stream = TitleContainer.OpenStream ("Content/charactersheet.png"))
-					      {
-					          characterSheetTexture = Texture2D.FromStream (graphicsDevice, stream);
-					      }
-					  }
-						*/
+            if (characterSheetTexture == null)
+          {
+              using (var stream = TitleContainer.OpenStream ("Content/charactersheet.png"))
+              {
+                  characterSheetTexture = Texture2D.FromStream (graphicsDevice, stream);
+              }
+          }
+            */
 
-						JumpUp = new Animation();
-						JumpUp.AddFrame(new Rectangle (50, 100, 20, 20), TimeSpan.FromSeconds (.2));
-						JumpUp.AddFrame(new Rectangle (50, 90, 20, 20), TimeSpan.FromSeconds (.2));
-						JumpUp.AddFrame(new Rectangle (50, 80, 20, 20), TimeSpan.FromSeconds (.2));
-						JumpUp.AddFrame(new Rectangle (50, 70, 20, 20), TimeSpan.FromSeconds (.1));
-						JumpUp.AddFrame(new Rectangle (50, 85, 20, 20), TimeSpan.FromSeconds (.2));
-						JumpUp.AddFrame(new Rectangle (50, 100, 20, 20), TimeSpan.FromSeconds (.2));
+            JumpUp = new Animation();
+            JumpUp.AddFrame(new Rectangle(50, 100, 20, 20), TimeSpan.FromSeconds(.2));
+            JumpUp.AddFrame(new Rectangle(50, 90, 20, 20), TimeSpan.FromSeconds(.2));
+            JumpUp.AddFrame(new Rectangle(50, 80, 20, 20), TimeSpan.FromSeconds(.2));
+            JumpUp.AddFrame(new Rectangle(50, 70, 20, 20), TimeSpan.FromSeconds(.1));
+            JumpUp.AddFrame(new Rectangle(50, 85, 20, 20), TimeSpan.FromSeconds(.2));
+            JumpUp.AddFrame(new Rectangle(50, 100, 20, 20), TimeSpan.FromSeconds(.2));
 
         }
 
-				public void Jump(){
-					if(!isJumping){
+        public Rectangle BoundingBox => new Rectangle();
+        public bool isMoving => true;
 
-					}
-					isJumping = true;
-				}
+        public void Jump()
+        {
+            if (!isJumping)
+            {
 
-				public void Update(GameTime gametime){
-          if (isJumping)
-          {
-              if (heightJumped < maxJumpHeight)
-              {
-                  heightJumped = heightJumped + 10;
-              }
-              else
-              {
-                  isJumping = false;
-              }
-          }else if(!isJumping && heightJumped > 0)
-          {
-              heightJumped = heightJumped - 10;
-          }
+            }
+            isJumping = true;
+        }
 
-					CurrentAnimation = JumpUp;
-					CurrentAnimation.Update(gametime);
-				}
+        public void Update(GameTime gametime)
+        {
+            if (isJumping)
+            {
+                if (heightJumped < maxJumpHeight)
+                {
+                    heightJumped = heightJumped + 10;
+                }
+                else
+                {
+                    isJumping = false;
+                }
+            }
+            else if (!isJumping && heightJumped > 0)
+            {
+                heightJumped = heightJumped - 10;
+            }
 
-				public void Draw(SpriteBatch spriteBatch){
-					
-				}
+            CurrentAnimation = JumpUp;
+            CurrentAnimation.Update(gametime);
+        }
 
+        public void Draw(SpriteBatch spriteBatch)
+        {
 
+        }
+
+        public void CollisionResponse(bool isItem)
+        {
+
+        }
     }
 }
