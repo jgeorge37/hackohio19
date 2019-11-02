15cd0fcda25e3c4340c10b00ebee97a60f94d766
jgeorge37 <george.921@osu.edu>
2019-11-02 18:10:02 -0400

    rectangle time

--- FinalAttempt/Game8/Animate.cs
+++ FinalAttempt/Game8/Animate.cs
@@ -49,5 +49,46 @@ namespace Game8
 
             timeIntoAnimation = TimeSpan.FromSeconds (remainder);
         }
+
+				public Rectangle CurrentRectangle
+				{
+				  get
+				  {
+				      AnimationFrame currentFrame = null;
+
+				      // See if we can find the frame
+				      TimeSpan accumulatedTime = new TimeSpan();
+				      foreach(var frame in frames)
+				      {
+				          if (accumulatedTime + frame.Duration >= timeIntoAnimation)
+				          {
+				              currentFrame = frame;
+				              break;
+				          }
+				          else
+				          {
+				              accumulatedTime += frame.Duration;
+				          }
+				      }
+
+				      // If no frame was found, then try the last frame,
+				      // just in case timeIntoAnimation somehow exceeds Duration
+				      if (currentFrame == null)
+				      {
+				          currentFrame = frames.LastOrDefault ();
+				      }
+
+				      // If we found a frame, return its rectangle, otherwise
+				      // return an empty rectangle (one with no width or height)
+				      if (currentFrame != null)
+				      {
+				          return currentFrame.SourceRectangle;
+				      }
+				      else
+				      {
+				          return Rectangle.Empty;
+				      }
+				  }
+				}
 		}
 }
