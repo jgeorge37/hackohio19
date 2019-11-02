a457346aa2e089f037fec96ec433febde1976fc1
jgeorge37 <george.921@osu.edu>
2019-11-02 18:34:30 -0400

    avatar animation???? needs help

--- FinalAttempt/Game8/Avatar.cs
+++ FinalAttempt/Game8/Avatar.cs
@@ -10,41 +10,70 @@ namespace Game8
 {
     class Avatar
     {
-		bool isJumping;
+				bool isJumping;
         int heightJumped;
         int maxJumpHeight;
+				Animation JumpUp;
+				Animation CurrentAnimation;
 
-        public Avatar()
-        {
-			isJumping = false;
-            heightJumped = 0;
+		    public Avatar(GraphicsDevice graphicsDevice)
+		    {
+						isJumping = false;
+        		heightJumped = 0;
             maxJumpHeight = 100;
+
+						/*
+						THIS IS STUFF FROM THE EXAMPLE THAT IDK HOW TO DEAL WITH
+
+						if (characterSheetTexture == null)
+					  {
+					      using (var stream = TitleContainer.OpenStream ("Content/charactersheet.png"))
+					      {
+					          characterSheetTexture = Texture2D.FromStream (graphicsDevice, stream);
+					      }
+					  }
+						*/
+
+						JumpUp = new Animation();
+						JumpUp.AddFrame(new Rectangle (50, 100, 20, 20), TimeSpan.FromSeconds (.2));
+						JumpUp.AddFrame(new Rectangle (50, 90, 20, 20), TimeSpan.FromSeconds (.2));
+						JumpUp.AddFrame(new Rectangle (50, 80, 20, 20), TimeSpan.FromSeconds (.2));
+						JumpUp.AddFrame(new Rectangle (50, 70, 20, 20), TimeSpan.FromSeconds (.1));
+						JumpUp.AddFrame(new Rectangle (50, 85, 20, 20), TimeSpan.FromSeconds (.2));
+						JumpUp.AddFrame(new Rectangle (50, 100, 20, 20), TimeSpan.FromSeconds (.2));
 
         }
 
-		public void Jump(){
-			if(!isJumping){
+				public void Jump(){
+					if(!isJumping){
 
-			}
-			isJumping = true;
-		}
+					}
+					isJumping = true;
+				}
 
-		public void Update(){
-            if (isJumping)
-            {
-                if (heightJumped < maxJumpHeight)
-                {
-                    heightJumped = heightJumped + 10;
-                }
-                else
-                {
-                    isJumping = false;
-                }
-            }else if(!isJumping && heightJumped > 0)
-            {
-                heightJumped = heightJumped - 10;
-            }
-		}
+				public void Update(GameTime gametime){
+          if (isJumping)
+          {
+              if (heightJumped < maxJumpHeight)
+              {
+                  heightJumped = heightJumped + 10;
+              }
+              else
+              {
+                  isJumping = false;
+              }
+          }else if(!isJumping && heightJumped > 0)
+          {
+              heightJumped = heightJumped - 10;
+          }
+
+					CurrentAnimation = JumpUp;
+					CurrentAnimation.Update(gametime);
+				}
+
+				public void Draw(SpriteBatch spriteBatch){
+					
+				}
 
 
     }
