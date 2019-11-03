2187142b408870a111f378f1b348c93095fa1989
jgeorge37 <george.921@osu.edu>
2019-11-02 20:09:49 -0400

    items and obstacles drawing

--- Stuff/Items.cs
+++ Stuff/Items.cs
@@ -11,15 +11,16 @@ namespace Game8.Stuff
 {
     class Items : ICollidable
     {
-        public Items()
+        Texture2D texture;
+        public Items(Texture2D tx)
         {
-
+          texture = tx;
         }
         public Rectangle BoundingBox => new Rectangle();
         public bool HasResponse => true;
         public void Draw(SpriteBatch spriteBatch)
         {
-
+          spriteBatch.Draw(texture, new Rectangle(200, 100, 30, 40), Color.White);
         }
         public void Update(GameTime gameTime)
         {
--- Stuff/Obstacles.cs
+++ Stuff/Obstacles.cs
@@ -11,15 +11,16 @@ namespace Game8.Stuff
 {
     class Obstacles : ICollidable
     {
-        public Obstacles()
+        Texture2D texture;
+        public Obstacles(Texture2D tx)
         {
-
+          texture = tx;
         }
         public Rectangle BoundingBox => new Rectangle();
         public bool HasResponse => false;
         public void Draw(SpriteBatch spriteBatch)
         {
-
+          spriteBatch.Draw(texture, new Rectangle(30, 200, 30, 40), Color.White);
         }
         public void Update(GameTime gameTime)
         {
